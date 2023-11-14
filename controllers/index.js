1af7a66235d0118bb3d45349e7e51985bcc0f201
hjlee <heunjong.lee@gmail.com>
2023-11-14 14:47:50 -0500

    connect front and back ends vis testRoute.js and test.handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
-// homeroutes?
-
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
+// for test landing page
+const testRoute = require('./testRoute');
+router.use('/', testRoute);
 
 module.exports = router;
