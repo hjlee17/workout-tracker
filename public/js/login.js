3b2352e260d0a84977aab679967fb278d00857f7
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2023-11-15 22:47:23 -0500

    login form jquery selectors handler

--- public/js/login.js
+++ public/js/login.js
@@ -3,3 +3,22 @@ const loginFormHandler = async (event) => {
     // const email = TODO:email jquery
     // const password = TODO:password jsquery
 
+    if (email && password) {
+        // Send the e-mail and password to the server
+        const response = await fetch('/api/users/login', {
+          method: 'POST',
+          body: JSON.stringify({ email, password }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+    
+        if (response.ok) {
+          document.location.replace('/');
+        } else {
+          alert('Failed to log in');
+        }
+      }
+    };
+document //TODO: Awaiting handlbar integration
+.querySelector('.login-form')
+.addEventListener('submit', loginFormHandler);
+  
