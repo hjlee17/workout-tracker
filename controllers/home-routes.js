8789520d9596e6fae953920c95b0541bae61b08d
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2023-11-15 23:20:07 -0500

    home-routes boilerplate

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,5 +2,20 @@ const router = require('express').Router();
 const { User } = require('../models');
 
 router.get('/', async (req, res) => {
-
-});
+    //logged in renders /user of logged in user
+    try {
+        const userData = await User.findAll({
+          attributes: { exclude: ['password'] },
+          order: [['name', 'ASC']],
+        });
+        const users = userData.map((project) => project.get({ plain: true }));
+        res.render('TODO', { //TODO: Find logged in 
+          users,
+          // Pass the logged in flag to the template
+          logged_in: req.session.logged_in,
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
+    
