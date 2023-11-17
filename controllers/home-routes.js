15e5578f651b85cde59b26d8f4607c73a3d3862d
hjlee17 <140988381+hjlee17@users.noreply.github.com>
2023-11-16 19:49:15 -0500

    Update home-routes.js
    TODO - need to render a view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,13 +3,15 @@ const { User } = require('../models');
 
 router.get('/', async (req, res) => {
     //logged in renders /user of logged in user
-    try {
+    try {\
+        // update below based on what we decide to pass into the TODO page
         const userData = await User.findAll({
           attributes: { exclude: ['password'] },
           order: [['name', 'ASC']],
         });
         const users = userData.map((project) => project.get({ plain: true }));
         res.render('TODO', { //TODO: Find logged in 
+          // update below based on what we decide to pass into the TODO page
           users,
           // Pass the logged in flag to the template
           logged_in: req.session.logged_in,
