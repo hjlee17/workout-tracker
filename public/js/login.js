545cc8980e9eee1328ca3c06097b180d2b6b7598
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2023-11-20 21:51:05 -0500

    whatever

--- public/js/login.js
+++ public/js/login.js
@@ -2,12 +2,12 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
     const email = $('#email-login').val().trim();
     const password = $('#password-login').val().trim();
-
+  
     if (email && password) {
         // Send the e-mail and password to the server
         const response = await fetch('/api/login', {
           method: 'POST',
-          body: JSON.stringify({ email, password }),
+          body: JSON.stringify({ email: email, password: password }),
           headers: { 'Content-Type': 'application/json' },
         });
     
@@ -18,6 +18,13 @@ const loginFormHandler = async (event) => {
         }
       }
     };
-document 
-.querySelector('#login-btn')
-.addEventListener('submit', loginFormHandler);
+// event handler for the login button
+$('#login-btn').click(function(event) {
+  loginHandler(event);
+});
+// event handler for enter key on the last input field
+$('#password').on('keyup', function(event) {
+  if (event.key === 'Enter') {
+    loginHandler(event);
+  }
+});
