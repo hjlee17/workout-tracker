2063588847b5bc5be56162e69f437784a76a3173
Fluffy <mrsargent00@gmail.com>
2023-11-20 21:45:53 -0500

    updated code

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,61 +1,56 @@
 // Prevents the default form submission behavior to handle the signup process asynchronously
 const signupFormhandler = async (event) => {
-  event.preventDefault();
-
-
-// Retrieves the input elements for username and password from the DOM
-const usernameImput = document.querySelector("#username-signup");
-const passwordImput = document.querySelector('#password-signup');
-
-// Retrieves and trims the values of username and password
-const username = usernameImput.value.trim();
-const password = passwordImput.value.trim();
-
-// Checks if both username and password are provided
-if (username && password) {
-  // Creates an object with user data
-  const userData = {
-      username,
-      password,
-  }; 
-
-  try {
-      // Sends a POST request to the "/api/users" endpoint with user data
-      const response = await fetch("/api/users", {
+    event.preventDefault();
+  
+    // Retrieves the input elements for username and password from the DOM
+    const usernameInput = document.querySelector("#username-signup");
+    const passwordInput = document.querySelector("#password-signup");
+  
+    // Retrieves and trims the values of username and password
+    const username = usernameInput.value.trim();
+    const password = passwordInput.value.trim();
+  
+    // Checks if both username and password are provided
+    if (username && password) {
+      // Creates an object with user data
+      const userData = {
+        username,
+        password,
+      };
+  
+      try {
+        // Sends a POST request to the "/api/users" endpoint with user data
+        const response = await fetch("/api/users", {
           method: "POST",
           body: JSON.stringify(userData),
           headers: {
-              "Content-Type": "application/json",
+            "Content-Type": "application/json",
           },
-      });
-
-      // Checks if the response is successful
-      if (response.ok) {
+        });
+  
+        // Checks if the response is successful
+        if (response.ok) {
           // Redirects to the dashboard upon successful signup
           document.location.replace("/dashboard");
-      } else {
+        } else {
           // Displays an error message if signup fails
           const errorMessage = await response.text();
           alert(`Signup failed: ${response.statusText}\n${errorMessage}`);
+        }
+      } catch (error) {
+        // Handles errors that occur during the asynchronous operation
+        console.error(`An error occurred: ${error.message}`);
       }
-  } catch (error) {
-      // Handles errors that occur during the asynchronous operation
-      console.error(`An error occurred: ${error.message}`);
-  }
-}
-}; 
-
-// Adds a submit event listener to the signup form, triggering the signupFormhandler function
-// document
-//   .querySelector(".signup-form")
-//   .addEventListener("submit", signupFormhandler);
-const buttonTest = async (event) => {
-    event.preventDefault();
-    console.log("button clicked")
-    // document.location.replace('/test');
-  }
+    }
+  };
   
-  // event handler for the edit post button
-  $('#signUp-btn').click(function(event) {
-    buttonTest(event);
+  // Adds a submit event listener to the signup form, triggering the signupFormhandler function
+  document
+    .querySelector(".signup-form")
+    .addEventListener("submit", signupFormhandler);
+  
+  // Event handler for the sign-up button (assuming it has the ID 'signUp-btn')
+  $('#signUp-btn').click(function (event) {
+    signupFormhandler(event);
   });
+  
